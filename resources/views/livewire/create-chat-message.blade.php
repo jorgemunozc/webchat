c4a82e40014774ac4d30081d9aa185699401341f
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2023-10-18 17:19:32 -0300

    refactor: save message is handled in form

--- resources/views/livewire/create-chat-message.blade.php
+++ resources/views/livewire/create-chat-message.blade.php
@@ -1,8 +1,7 @@
 <div class="border">
-    {{-- <div>
-        <p>Chat: {{ $chatId }}</p>
-    </div> --}}
-    <input type="text" id="message" wire:model="message" class="bg-gray-100">
-    <button wire:click="save" class="bg-blue-400 text-white w-32">Send</button>
-    <div >Message: {{ $message }}</div>
+    <form wire:submit="save">
+        <input type="text" id="message" wire:model="message" class="bg-gray-100">
+        <button type="submit" class="bg-blue-400 text-white w-32">Send</button>
+    </form>
+
 </div>
